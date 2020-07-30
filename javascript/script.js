6819d69f9f3f647757d7c1abca0b2dd4733e5938
gustavoisaacc <gustavochaile7@gmail.com>
2020-07-30 01:55:54 -0300

    areglando el menu y agregandole un activo para saber dond estamos en la navegacion

--- javascript/script.js
+++ javascript/script.js
@@ -22,14 +22,21 @@ setTimeout(typeWriter, speed);
 const menu = document.querySelectorAll('.menu a');
 
 
-menu.forEach(function(item){
+menu.forEach(function(item,i){
     item.addEventListener('click', function(e){
         e.preventDefault();
-     
+        const enlace = item.getAttribute('href').slice(1)
+        const sobreMI = document.getElementById(enlace)
+
+        
+        const menus = document.querySelectorAll('nav a')
+        menus.forEach(m => m.classList.remove('activo'))  
+        menus[i].classList.add('activo')
+
         const datos = document.querySelectorAll('.sobre-mi')
-        datos.forEach(function(info){
-            info.classList.toggle('active');
-        })    
+        datos.forEach(info => info.classList.remove('show'))    
+
+        sobreMI.classList.add('show')
 
     })
 
